e59ffe5983129cfbddf122151ed37b082c81a4f6
Huyen-le-0504 <huyen.le@abcdigital.io>
2026-07-13 20:23:06 +0700

    PR11072026 (#5)
    * PR11072026
    
    Verify text + dropdown list
    
    * Edit code (verify text + dropdown list)

--- src/steps/dashboard/dashboardSteps.ts
+++ src/steps/dashboard/dashboardSteps.ts
@@ -1,8 +1,8 @@
-import { Given } from "@cucumber/cucumber";
-import { BaseDashboard } from "../../pages/dashboard/dashboardPage";
-import { CustomWorld } from "../../support/world";
+// import { Given } from "@cucumber/cucumber";
+// import { BaseDashboard } from "../../pages/dashboard/dashboardPage";
+// import { CustomWorld } from "../../support/world";
 
-Given("User opens the dashboard page", async function (this: CustomWorld) {
-  const dashboardPage = new BaseDashboard(this.page);
-  await dashboardPage.navigateTodashboardPage();
-});
+// Given("User opens the dashboard page", async function (this: CustomWorld) {
+//   const dashboardPage = new BaseDashboard(this.page);
+//   await dashboardPage.navigateTodashboardPage();
+// });
